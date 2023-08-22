0449cdbabe76d208c1343f99b62ecb21b82bc096
Bayram Akbuz <bayram.akbuz@gmail.com>
2023-08-22 15:02:41 +0300

    opentelemetry versions upgraded

--- common-service/go.mod
+++ common-service/go.mod
@@ -4,34 +4,36 @@ go 1.21
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/rs/zerolog v1.27.0
-	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.32.0
-	go.opentelemetry.io/otel v1.7.0
-	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.30.0
-	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.30.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.7.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.7.0
-	go.opentelemetry.io/otel/metric v0.30.0
-	go.opentelemetry.io/otel/sdk v1.7.0
-	go.opentelemetry.io/otel/sdk/metric v0.30.0
-	go.opentelemetry.io/otel/trace v1.7.0
-	google.golang.org/grpc v1.47.0
-	google.golang.org/protobuf v1.28.0
+	github.com/rs/zerolog v1.30.0
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.42.0
+	go.opentelemetry.io/otel v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v0.39.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.16.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.16.0
+	go.opentelemetry.io/otel/sdk v1.16.0
+	go.opentelemetry.io/otel/sdk/metric v0.39.0
+	go.opentelemetry.io/otel/trace v1.16.0
+	google.golang.org/grpc v1.57.0
+	google.golang.org/protobuf v1.31.0
 )
 
 require (
-	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	cloud.google.com/go/compute v1.23.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.3 // indirect
-	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.7.0 // indirect
-	go.opentelemetry.io/proto/otlp v0.16.0 // indirect
-	golang.org/x/net v0.0.0-20220607020251-c690dde0001d // indirect
-	golang.org/x/sys v0.0.0-20220608164250-635b8c9b7f68 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220608133413-ed9918b62aac // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.16.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric v0.39.0 // indirect
+	go.opentelemetry.io/otel/metric v1.16.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto v0.0.0-20230821184602-ccc8af3d0e93 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230821184602-ccc8af3d0e93 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230821184602-ccc8af3d0e93 // indirect
 )
--- common-service/telem/telem.go
+++ common-service/telem/telem.go
@@ -6,73 +6,31 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
-	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/propagation"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"google.golang.org/grpc"
 )
 
 // Initializes an OTLP exporter, and configures the corresponding trace and metric providers.
 func InitProvider(ctx context.Context, otelAgentAddr string, serviceNameKey string) func() {
-	log.Println("opentelemetry initializing 1/10:", otelAgentAddr, serviceNameKey)
+	// step 1
+	log.Println("opentelemetry initializing 1/8:", otelAgentAddr, serviceNameKey)
 
-	metricClient := otlpmetricgrpc.NewClient(
-		otlpmetricgrpc.WithInsecure(),
-		otlpmetricgrpc.WithEndpoint(otelAgentAddr))
-
-	log.Println("opentelemetry initializing 2/10: exporter metric client initializing")
-
-	metricExp, err := otlpmetric.New(ctx, metricClient)
-	if err != nil {
-		log.Fatalf("failed to create the collector metric exporter: %s", err)
-	}
-
-	log.Println("opentelemetry initializing 3/10: exporter metric controller initializing")
-
-	pusher := controller.New(
-		processor.NewFactory(simple.NewWithHistogramDistribution(), metricExp),
-		controller.WithExporter(metricExp),
-		controller.WithCollectPeriod(2*time.Second),
-	)
-	global.SetMeterProvider(pusher)
-
-	log.Println("opentelemetry initializing 4/10: exporter metric controller starting")
-
-	if err = pusher.Start(ctx); err != nil {
-		log.Fatalf("failed to start metric pusher: %s", err)
-	}
-
-	log.Println("opentelemetry initializing 5/10: grpc trace client initializing")
-
-	traceClient := otlptracegrpc.NewClient(
-		otlptracegrpc.WithInsecure(),
-		otlptracegrpc.WithEndpoint(otelAgentAddr),
-		otlptracegrpc.WithDialOption(grpc.WithBlock()))
-
-	log.Println("opentelemetry initializing 6/10: grpc trace exporter initializing -> ", otelAgentAddr)
-
-	traceExp, err := otlptrace.New(ctx, traceClient)
-	if err != nil {
-		log.Fatalf("failed to create the collector trace exporter: %s", err)
-	}
-
-	log.Println("opentelemetry initializing 7/10: trace resource configuring")
-
+	// step 2
+	log.Println("opentelemetry initializing 2/8: resource initializing")
 	res, err := resource.New(ctx,
 		resource.WithFromEnv(),
 		resource.WithProcess(),
 		resource.WithTelemetrySDK(),
 		resource.WithHost(),
 		resource.WithAttributes(
+			// the service name used to display traces in backends
 			semconv.ServiceNameKey.String(serviceNameKey),
 		),
 	)
@@ -80,7 +38,52 @@ func InitProvider(ctx context.Context, otelAgentAddr string, serviceNameKey stri
 		log.Fatalf("failed to create resource: %s", err)
 	}
 
-	log.Println("opentelemetry initializing 8/10: tracer provider initializing")
+	// step 3
+	log.Println("opentelemetry initializing 3/8: metric exporter initializing")
+
+	metricExp, err := otlpmetricgrpc.New(
+		ctx,
+		otlpmetricgrpc.WithInsecure(),
+		otlpmetricgrpc.WithEndpoint(otelAgentAddr),
+	)
+	if err != nil {
+		log.Fatalf("failed to create the collector metric exporter: %s", err)
+	}
+
+	// step 4
+	log.Println("opentelemetry initializing 4/8: meter provider initializing")
+
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithResource(res),
+		sdkmetric.WithReader(
+			sdkmetric.NewPeriodicReader(
+				metricExp,
+				sdkmetric.WithInterval(2*time.Second),
+			),
+		),
+	)
+	otel.SetMeterProvider(meterProvider)
+
+	// step 5
+	log.Println("opentelemetry initializing 5/8: grpc trace client initializing")
+
+	traceClient := otlptracegrpc.NewClient(
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint(otelAgentAddr),
+		otlptracegrpc.WithDialOption(grpc.WithBlock()))
+	sctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+
+	// step 6
+	log.Println("opentelemetry initializing 6/8: trace exporter initializing")
+
+	traceExp, err := otlptrace.New(sctx, traceClient)
+	if err != nil {
+		log.Fatalf("failed to create the collector trace exporter: %s", err)
+	}
+
+	// step 7
+	log.Println("opentelemetry initializing 7/8: tracer provider initializing")
 
 	bsp := sdktrace.NewBatchSpanProcessor(traceExp)
 	tracerProvider := sdktrace.NewTracerProvider(
@@ -88,30 +91,22 @@ func InitProvider(ctx context.Context, otelAgentAddr string, serviceNameKey stri
 		sdktrace.WithResource(res),
 		sdktrace.WithSpanProcessor(bsp),
 	)
-
-	log.Println("opentelemetry initializing 9/10: tracer provider initialized")
-
 	// set global propagator to tracecontext (the default is no-op).
-	otel.SetTextMapPropagator(propagation.TraceContext{})
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 	otel.SetTracerProvider(tracerProvider)
 
-	log.Println("opentelemetry initializing 10/10: opentelemetry initialized")
+	// step 8
+	log.Println("opentelemetry initializing 8/8: opentelemetry initialized")
 
 	return func() {
-		log.Println("opentelemetry starting")
-
 		cxt, cancel := context.WithTimeout(ctx, time.Second)
 		defer cancel()
 		if err := traceExp.Shutdown(cxt); err != nil {
-			log.Fatalf("failed to otlp shutdown: %s", err)
 			otel.Handle(err)
 		}
 		// pushes any last exports to the receiver
-		if err := pusher.Stop(cxt); err != nil {
-			log.Fatalf("failed to pusher stop: %s", err)
+		if err := meterProvider.Shutdown(cxt); err != nil {
 			otel.Handle(err)
 		}
-
-		log.Println("opentelemetry started")
 	}
 }
